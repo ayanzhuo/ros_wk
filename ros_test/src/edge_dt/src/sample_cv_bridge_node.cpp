1f1932d6cc9c70d9b98151ec93d80af9f9c22cc7
ayanzhuo <2264439821@qq.com>
2024-01-03 17:20:09 +0000

    666

--- ros_test/src/edge_dt/src/sample_cv_bridge_node.cpp
+++ ros_test/src/edge_dt/src/sample_cv_bridge_node.cpp
@@ -1,9 +1,4 @@
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include "main.h"
  
 static const std::string OPENCV_WINDOW = "Raw Image window";
 static const std::string OPENCV_WINDOW_1 = "Edge Detection";
